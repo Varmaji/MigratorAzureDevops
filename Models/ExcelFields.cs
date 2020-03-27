dccb7b58981cee2e8f080bbe4e4229c2486522b1
Varmaji <92varma@gmail.com>
2020-03-27 12:46:07 +0530

    Add project files.

---
+++ Models/ExcelFields.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MigratorAzureDevops.Models
+{
+    public class ExcelFields
+    {
+        public string ID { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Title3 { get; set; }
+        public string AreaPath { get; set; }
+        public string Iteration { get; set; }
+        //public string EpicCategory; //Epic Category
+        //public string Themes;
+        //public string Frameworks;
+        //public string EpicType;      //Epic Type
+        //public string RequirementSource;  //Requirement Source
+        //public string CLMCreatedBy;   //CLM Created By
+        //public string CLMPlannedFor;  //CLM Planned For
+        //public string Bot_or_framework;
+        //public string CLM_Ownedby; //CLM_Ownedby
+        //public string CLM_ID;
+        //public string CLM_Priority;
+        //public string CLM_FiledAgainst;
+        //public string StoryOrigin;  //Story Origin
+        //public string TaskCategory; //Task Category
+        //public string OriginalEstimate;   //Original Estimate
+        //public string CompletedWork; //Completed Work
+        //public string WorkItemType;  // Work Item Type
+        //public string State;
+        //public string AssignedTo;
+        //public string StoryPoint;
+        //public string Description;
+
+
+
+
+    }
+}
---
+++ Models/ExcelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MigratorAzureDevops.Models
+{
+    public class ExcelModel
+    {
+        public List<string> sheetName { get; set; }
+    }
+}
